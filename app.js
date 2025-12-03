c0a029bf0f9289d894de27af604a83fd7f00012d
Aryeahhh <aryapatel0107@gmail.com>
2025-12-02 22:20:36 -0500

    vercel fix#6

--- app.js
+++ app.js
@@ -144,3 +144,6 @@ if (process.env.NODE_ENV !== 'production') {
   const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 }
+app.use('*', (req, res) => {
+    res.sendFile(`${__dirname}/public/index.html`);
+});
